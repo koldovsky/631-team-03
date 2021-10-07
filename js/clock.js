1398fcf6f9eb14acfc7e2fc36ca4430302440fe4
Lanamc <yulyamc87@gmail.com>
2021-10-07 16:47:31 +0300

    Added countdown

--- js/clock.js
+++ js/clock.js
@@ -1,23 +1,20 @@
-const clockContainer = document.querySelector('.clock');
-    setInterval(updateClock, 1000);
-    
-    function updateClock() { 
-    
-    // Seconds
-    const seconds = new Date().getSeconds();
-    document.getElementById("seconds").innerHTML = (seconds < 10 ? '0' : '') + seconds;
+const countDownDate = new Date("Nov 1 2021 00:00:00").getTime();
+ 
+  let x = setInterval(function() {
 
-    // Minutes
-    const minutes = new Date().getMinutes();
-    document.getElementById("minutes").innerHTML = (minutes < 10 ? '0' : '') + minutes;
-
-    // Hours
-    const hours = new Date().getHours();
-    document.getElementById("hours").innerHTML = (hours < 10 ? '0' : '') + hours;
+  let now = new Date().getTime();
   
-    
-    //Days
-    const days = new Date().getDays();
-    document.getElementById("days").innerHTML = (days < 10 ? '0' : '') + days;
-  clockContainer.innerText = newDate().toLocaleTimeString();
-}
+  let distance = countDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+    document.getElementById("time-days").innerHTML = days 
+    document.getElementById("time-hours").innerHTML = hours
+    document.getElementById("time-minutes").innerHTML = minutes
+    document.getElementById("time-seconds").innerHTML = seconds
+      
+   }, 1000);
+
